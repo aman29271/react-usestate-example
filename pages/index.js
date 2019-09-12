9e84f53e41114b305657fe86bfc6da33bb3a1e9c
Aman <aman29271@gmail.com>
2019-09-12 19:15:44 +0530

    deleted previous index

--- pages/index.js
+++ pages/index.js
@@ -1,91 +1,34 @@
-import React from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
-import Nav from '../components/nav'
-
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-    </Head>
-
-    <Nav />
-
-    <div className='hero'>
-      <h1 className='title'>Welcome to Next.js!</h1>
-      <p className='description'>
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-
-      <div className='row'>
-        <Link href='https://github.com/zeit/next.js#getting-started'>
-          <a className='card'>
-            <h3>Getting Started &rarr;</h3>
-            <p>Learn more about Next.js on GitHub and in their examples</p>
-          </a>
-        </Link>
-        <Link href='https://github.com/zeit/next.js/tree/master/examples'>
-          <a className='card'>
-            <h3>Examples &rarr;</h3>
-            <p>Find other example boilerplates on the Next.js GitHub</p>
-          </a>
-        </Link>
-        <Link href='https://github.com/zeit/next.js'>
-          <a className='card'>
-            <h3>Create Next App &rarr;</h3>
-            <p>Was this tool helpful? Let us know how we can improve it!</p>
-          </a>
-        </Link>
+import React, { useState } from "react";
+import InputText from "../components/InputText";
+import ListPost from "../components/ListPost";
+import Head from "next/head";
+import "bulma/bulma.sass";
+const Main = () => {
+  const [post, setPost] = useState([]);
+  const updatePost = (prv, newState) => {
+    const newPost = [...prv, newState];
+    setPost(newPost);
+  };
+  const removePost = item => {
+    const newPost = post.filter(p => p != item);
+    setPost(newPost);
+  };
+  return (
+    <section className="section">
+      <Head>
+        <title>Simple MVC React App</title>
+      </Head>
+      <div className="container">
+        <div className="columns">
+          <div className="column" />
+          <div className="column">
+            <InputText post={post} addPost={updatePost} />
+            <ListPost post={post} removePost={removePost} />
+          </div>
+          <div className="column" />
+        </div>
       </div>
-    </div>
-
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
-
-export default Home
+    </section>
+  );
+};
+export default Main;
